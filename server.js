2d8cfc9905b4b095cc6b1dce199d0c7b05ea8309
Martin Clementz <martincclementz@gmail.com>
2023-02-22 23:15:37 +0100

    basic disco

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import path from 'path';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+const io = new Server(server);
+const __dirname = path.resolve(path.dirname(''));
+
+io.on('connection', (socket) => {
+  console.log('user connected');
+
+  // listen for color change event from the client
+  socket.on('color:update', (color) => {
+    // send the color change event to all connected clients
+    io.emit('color:update', color);
+  });
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+});
+
+// serve static files
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname });
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
